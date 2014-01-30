bdce5cf45ffd8722aa69c60556d3dfbc178e5965
asadsaifi <asad_saifi1@yahoo.com>
2014-01-30 10:00:51 +0500

    asad test
    asad test

--- DB/acs.sql
+++ DB/acs.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 2.11.7
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 30, 2014 at 05:41 AM
--- Server version: 5.5.34
--- PHP Version: 5.4.22
+-- Host: localhost
+-- Generation Time: Jan 30, 2014 at 09:54 AM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,19 +26,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `students` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL auto_increment,
   `Name` varchar(30) NOT NULL,
   `GPA` float NOT NULL,
-  PRIMARY KEY (`ID`)
+  `class` varchar(255) NOT NULL,
+  `techer` varchar(255) NOT NULL,
+  PRIMARY KEY  (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`ID`, `Name`, `GPA`) VALUES
-(1, 'Shahbaz', 3.14),
-(2, 'Asad', 4.14);
+INSERT INTO `students` (`ID`, `Name`, `GPA`, `class`, `techer`) VALUES
+(1, 'Shahbaz', 3.14, '', ''),
+(2, 'Asad', 4.14, '', '');
 
 -- --------------------------------------------------------
 
@@ -48,11 +49,11 @@ INSERT INTO `students` (`ID`, `Name`, `GPA`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `subjects` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL auto_increment,
   `Name` varchar(30) NOT NULL,
   `Code` varchar(30) NOT NULL,
   `Department` varchar(30) NOT NULL,
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY  (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
@@ -63,6 +64,24 @@ INSERT INTO `subjects` (`ID`, `Name`, `Code`, `Department`) VALUES
 (1, 'Web Programming', '326', 'CS'),
 (2, 'SE', '456', 'IT');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`id`, `name`, `telephone`) VALUES
+(0, 'asad', '67786876786'),
+(2, 'asad', '67786876786'),
+(3, 'ali', '5765765785');
